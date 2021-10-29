ea89433692f338dccd87ca80a6783aebdf5bbf89
jamilrayhan <rjamil336@gmail.com>
2021-10-29 23:24:23 +0600

    backend

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2otfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    // console.log("success");
+    // console.log("success async function");
+    const database = client.db("traveling");
+    const servicesCollection = database.collection("travelingServices");
+
+    // get api
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // get single id
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+    // post api
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      //   console.log(service);
+      const result = await servicesCollection.insertOne(service);
+      //   console.log(result);
+      res.json(result);
+    });
+
+    // delete api
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    });
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("this is backend");
+});
+
+app.listen(port, () => {
+  console.log("running port is : ", port);
+});
